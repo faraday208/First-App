9ebc0de63331c16765308be15a5b6b3f828d78d4
Ahmet Faruk Alaçam <ahmetalacam@modsoft.com.tr>
2021-12-16 22:46:04 +0300

    End of the project

--- FirstApp/MainPage.xaml.cs
+++ FirstApp/MainPage.xaml.cs
@@ -10,9 +10,16 @@ namespace FirstApp
 {
     public partial class MainPage : ContentPage
     {
+        private int count = 0;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
         }
     }
 }
